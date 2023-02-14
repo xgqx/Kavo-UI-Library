d8e1aa8c8ceb943ecad885ea18a8864b20a3d5dd
xgqx <99144547+xgqx@users.noreply.github.com>
2023-02-13 23:44:17 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -85,11 +85,11 @@ local themeStyles = {
         ElementColor = Color3.fromRGB(74, 58, 84)
     },
     Ocean = {
-        SchemeColor = Color3.fromRGB(86, 76, 251),
-        Background = Color3.fromRGB(26, 32, 58),
-        Header = Color3.fromRGB(38, 45, 71),
-        TextColor = Color3.fromRGB(200, 200, 200),
-        ElementColor = Color3.fromRGB(38, 45, 71)
+        SchemeColor = Color3.fromRGB(198, 181, 226),
+        Background = Color3.fromRGB(201, 255, 255),
+        Header = Color3.fromRGB(5, 5, 5),
+        TextColor = Color3.fromRGB(255,255,255),
+        ElementColor = Color3.fromRGB(20, 20, 20)
     },
     Midnight = {
         SchemeColor = Color3.fromRGB(26, 189, 158),
